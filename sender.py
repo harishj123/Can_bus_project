f503abccbcf0a2e3151bc87de855f980ec88a65a
Harish J <162663198+harishj123@users.noreply.github.com>
2026-06-19 21:15:41 +0530

    Add initial sender.py with CAN message definitions

---
+++ sender.py
@@ -0,0 +1,11 @@
+VERSION ""
+
+NS_ :
+
+BS_:
+
+BU_: SensorNode ECUNode
+
+BO_ 100 VehicleData: 2 SensorNode
+ SG_ Speed : 0|8@1+ (1,0) [0|255] "kmph" ECUNode
+ SG_ Temp  : 8|8@1+ (1,0) [0|255] "C" ECUNode
